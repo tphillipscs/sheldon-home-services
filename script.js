960cfdf17d9cdf89e436712f009332ef8a58813a
trevor <phillitr@oregonstate.edu>
2025-11-08 15:08:40 -0700

    Base site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll "Back to Top" behavior (redundant but safe for older browsers)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
